96f4f72725cfb93c5fb6e24325a54b5385870191
rummolprod999 <rummolprod999@gmail.com>
2018-07-04 10:56:07 +0300

    init

---
+++ Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using ParserTenders;
+namespace TestProjectTenders
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestPrice()
+        {
+            decimal d = 20979288.57m;
+            decimal m = UtilsFromParsing.ParsePrice("20979288,57");
+            Assert.AreEqual(d, m);
+        }
+    }
+}
